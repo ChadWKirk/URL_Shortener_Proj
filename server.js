378b97419c5d5ac334fa21d49b5d3739c1640bba
Chad Kirk <santaomgprod@gmail.com>
2022-07-31 00:04:58 -0500

    changed form input name from fullURL to fullUrl. Works now.

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const ShortUrl = require('./models/shortUrl');
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1/urlShortener', {  //connect to MongoDB database
+mongoose.connect('mongodb://localhost/urlShortener', {  //connect to MongoDB database
     useNewUrlParser: true, useUnifiedTopology: true
 });
 
-app.use('/public', express.static('public')); //instead of app.use(express.static('public'));
+app.use('/public', express.static('public')); //instead of app.use(express.static('public')); for CSS
 
 app.set('view engine', 'ejs'); //use ejs
 app.use(express.urlencoded({ extended: false }));
@@ -17,14 +17,13 @@ app.get('/', async (req, res) => {
     res.render('index', { shortUrls: shortUrls }); //get index.ejs on the screen
 });
 
-app.post('/shortUrls', async (req, res) => {
-   await ShortUrl.create({ full: req.body.fullUrl })
+app.post('/shortUrls', async (req, res) => {    //submit button posts to this
+   await ShortUrl.create({ full: req.body.fullUrl })    //creaate short url
    res.redirect('/')
 });
 
 app.get('/:shortUrl', async (req, res) => {
    const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-
    if (shortUrl == null) return res.sendStatus(404);
 
    shortUrl.clicks++;
